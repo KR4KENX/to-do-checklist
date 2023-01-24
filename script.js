5049652924d4525a61cfdff23079956dfcd14967
adriank64216 <sqat1024@gmail.com>
2023-01-24 17:32:31 +0100

    1.0v

---
+++ script.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+    addListenerOnTasks();
+}
+function addListenerOnTasks(){
+    document.querySelectorAll(".task > input").forEach((element) => {
+        element.addEventListener("change",()=>{
+            element.parentElement.remove();
+        })
+    });
+}
+
+function createTask(){
+    let newTask = document.getElementById("newTask");
+    let main = document.getElementsByTagName("main")[0];
+
+    let newTaskEl = document.createElement("div");
+
+    newTaskEl.className = "task";
+    newTaskEl.innerHTML = `<input type='checkbox' name='isDone' id='isDone'> <p>${newTask.value}</p>`
+    
+    main.appendChild(newTaskEl);
+    addListenerOnTasks();
+}
